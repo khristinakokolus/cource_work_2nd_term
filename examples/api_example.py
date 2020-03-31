64cd744469e0ddc57869813bbc038f2417536c8d
Khristina Kokolus <60686300+khristinakokolus@users.noreply.github.com>
2020-03-31 22:15:35 +0300

    Add files via upload

---
+++ examples/api_example.py
@@ -0,0 +1,48 @@
+"""This module represents the function that gets info from API"""
+import json
+import requests
+
+# in the files meal_generate_example.json, products_example.json
+# upc_products_example.json, recipes_example.json, complex_example.json
+# are the examples of function calls with different parameters
+# to get more information about them and their use to call the function
+# visit https://spoonacular.com/food-api/docs
+
+
+BASE_URL = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+HEADERS = {
+    'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+    'x-rapidapi-key': "..."
+}
+
+
+def get_information_from_url(base_url, add_url, options, file_name):
+    """
+    (str, str, str, str) -> None
+
+    Example of information retrieval about food using API.
+    This function has four main parameters.
+    Parameter base_url is the main site from where API comes from.
+    Parameter add_url is an extension of the url address where you
+    want to find information. Example: '/recipes/mealplans/generate'
+    Parameter options is responsible for the data that will be searched.
+    Example: {"timeFrame":"day","targetCalories":"2000","diet":"vegetarian",
+    "exclude":"shellfish%2C olives"}
+    Parameter file_name is for where you want to save the json data.
+    """
+    if isinstance(options, str):
+        url = base_url + add_url + '/' + options
+        response = requests.request("GET", url, headers=HEADERS,
+                                    params=options)
+    else:
+        url = base_url + add_url
+        response = requests.request("GET", url, headers=HEADERS,
+                                    params=options)
+    data = json.loads(response.text)
+    with open(file_name, 'w+') as js_file:
+        json.dump(data, js_file, indent=4)
+
+
+if __name__ == '__main__':
+    pass
+
