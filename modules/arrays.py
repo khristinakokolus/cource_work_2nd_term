58f431f515a5b4547028baeb467cd6574b31e475
Khristina Kokolus <60686300+khristinakokolus@users.noreply.github.com>
2020-05-11 23:18:51 +0300

    Create arrays.py
    data structure

---
+++ modules/arrays.py
@@ -0,0 +1,175 @@
+"""
+Module that represents the needed data structures for the investigation.
+That is mostly based on the materials from the labs.
+Using the book Data Structures and
+Algorithms Using Python – R. Necaise.
+"""
+import ctypes
+
+
+class Array:
+    """Creates an array with size elements"""
+    def __init__(self, size):
+        """(Array, int)
+
+        Creates the array structure using the ctypes module.
+        """
+        self._size = size
+        self._elements = (ctypes.py_object * size)()
+        self.clear(None)
+
+    def __len__(self):
+        """(Array) -> int
+
+        Returns the size of the array.
+        """
+        return self._size
+
+    def __getitem__(self, index):
+        """(Array, int) -> str
+
+        Gets the contents of the index element.
+        """
+        return self._elements[index]
+
+    def __setitem__(self, index, value):
+        """(Array, int, str)
+
+        Puts the value in the array element at index position.
+        """
+        self._elements[index] = value
+
+    def clear(self, value):
+        """(Array, str)
+
+        Clears the array by setting each element
+        to the given value.
+        """
+        for i in range(len(self)):
+            self._elements[i] = value
+
+    def __iter__(self):
+        """(Array)
+
+        Returns the array's iterator for traversing
+        the elements.
+        """
+        return _ArrayIterator(self._elements)
+
+    def __str__(self):
+        """
+        Returns the printable representation of array.
+        """
+        str_array = "["
+        for i in range(self._size):
+           str_array += str(self[i]) + ", "
+        str_array = str_array[:-2] + "]"
+        return str_array
+
+
+class _ArrayIterator:
+    """An iterator for the Array ADT."""
+    def __init__(self, the_array):
+        """(_ArrayIterator)
+
+        Represents the iterator.
+        """
+        self._array = the_array
+        self._cur_index = 0
+
+    def __iter__(self):
+        """(_ArrayIterator)
+
+        The iterator.
+        """
+        return self
+
+    def __next__(self):
+        """(_ArrayIterator)
+
+        The next value of iteration.
+        """
+        if self._cur_index < len(self._array):
+            entry = self._array[self._cur_index]
+            self._cur_index += 1
+            return entry
+        raise StopIteration
+
+
+class DynamicArray:
+    """A dynamic array class akin to a simplified Python list"""
+    def __init__(self):
+        """(DynamicArray)
+
+        Create an empty array.
+        """
+        self._num_elements = 0
+        self._capacity = 1
+        self._array = self.make_array(self._capacity)
+
+    def __len__(self):
+        """(DynamicArray)
+
+        Return number of elements stored in the array.
+        """
+        return self._num_elements
+
+    def __getitem__(self, index):
+        """(DynamicArray)
+
+        Return element at index.
+        """
+        return self._array[index]
+
+    def append(self, item):
+        """(DynamicArray)
+
+        Add object to end of the array.
+        """
+        if self._num_elements == self._capacity:
+            self.resize(2 * self._capacity)
+        self._array[self._num_elements] = item
+        self._num_elements += 1
+
+    def resize(self, capacity):
+        """(DynamicArray)
+
+        Resizes internal array to the certain capacity.
+        """
+        bigger_array = self.make_array(capacity)
+        for i in range(self._num_elements):
+            bigger_array[i] = self._array[i]
+        self._array = bigger_array
+        self._capacity = capacity
+
+    def make_array(self, capacity):
+        """(DynamicArray)
+
+        Return new array with capacity.
+        """
+        return (capacity * ctypes.py_object)()
+
+    def remove(self, value):
+        """(DynamicArray)
+
+        Remove first occurrence of value( or  raise ValueError).
+        """
+        for i in range(self._num_elements):
+            if self._array[i] == value:
+                for j in range(i, self._num_elements - 1):
+                    self._array[j] = self._array[j + 1]
+                self._array[self._num_elements - 1] = None
+                self._num_elements -= 1
+
+                return
+        raise ValueError("value not found")
+
+    def __str__(self):
+        """
+        Returns printable representation of DynamicArray.
+        """
+        str_dynamic_array = "["
+        for i in range(self._num_elements):
+           str_dynamic_array += str(self[i]) + ", "
+        str_dynamic_array = str_dynamic_array[:-2] + "]"
+        return str_dynamic_array
