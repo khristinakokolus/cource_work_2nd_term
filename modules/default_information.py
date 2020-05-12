4a0bf747c69a4afdef3c266a6e65bca2300717a3
Khristina Kokolus <60686300+khristinakokolus@users.noreply.github.com>
2020-05-12 06:37:26 +0300

    Add files via upload

---
+++ modules/default_information.py
@@ -0,0 +1,20 @@
+"""Module that contains needed default variables"""
+
+DEFAULT_MAIN_INDICATORS = ["Calories", "TimeCooking", "healthyScore"]
+
+CUISINES = ["None", "African", "American", "British", "Cajun", "Carribean", "Chinese",
+            "Eastern", "European", "French", "German", "Greek", "Indian", "Irish",
+            "Italian", "Japanese", "Jewish", "Korean", "Latin American", "Mediterranean",
+            "Mexican", "Middle Eastern", "Nordic", "Southern", "Spanish", "Thai", "Vietnamese"]
+
+DISH_TYPE = ["None", "main course", "side dish", "dessert", "appetizer", "salad", "bread",
+             "breakfast", "soup", "beverage", "sauce", "marinade", "fingerfood", "snack", "drink"]
+
+DIETS = ["None", "gluten free", "ketogenic", "vegetarian", "lacto vegetarian", "ovo vegetarian",
+         "vegan", "pescetarian", "paleo", "primal"]
+
+DEFAULT_MAIN_INFO = ["veryHealthy", "cheap", "veryPopular", "healthScore", "readyInMinutes", "sourceUrl",
+                     "cuisines", "dishTypes", "diets"]
+
+MAIN_SUBSTANCES = ["Fat", "Saturated Fat", "Carbohydrates",
+                   "Sugar", "Sodium", "Protein", "Fiber"]
