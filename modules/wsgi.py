0cef9dc349deb65dbfb55d875dd1e4b8c097c4ca
Khristina Kokolus <60686300+khristinakokolus@users.noreply.github.com>
2020-05-13 22:24:08 +0300

    Add files via upload

---
+++ modules/wsgi.py
@@ -0,0 +1,15 @@
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from werkzeug.serving import run_simple
+
+from modules.complex_dashboard import app as app1
+from modules.meal_gen_dashboard import app as app2
+from modules.flask_app import flask_app
+
+
+application = DispatcherMiddleware(flask_app, {
+    '/complex': app1.server,
+    '/mealgenerating': app2.server,
+})
+
+if __name__ == '__main__':
+    run_simple('localhost', 8050, application)
