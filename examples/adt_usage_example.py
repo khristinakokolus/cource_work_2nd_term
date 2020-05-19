cb2ec1b2c05b58802a04dd60fe961ac80b10f100
Khristina Kokolus <60686300+khristinakokolus@users.noreply.github.com>
2020-05-19 06:53:29 +0300

    Update adt_usage_example.py

--- examples/adt_usage_example.py
+++ examples/adt_usage_example.py
@@ -1,27 +1,20 @@
 """Module that represents the example of using RecipeSearch ADT and RecipeData ADT """
-import requests
-import json
-import plotly.express as px
-import pandas as pd
 from modules.recipe_search_adt import RecipeSearch
-from modules.recipe_advanced_adt import RecipeData
+from modules.recipe_data_adt import RecipeData
+from modules.main_functions import get_data_from_url, data_from_recipe_ids, check_data, plot_nutrition
 import modules.default_information
 import modules.errors
 
 # this module uses constant variables
 # exactly to show the work of RecipeSearch ADT and RecipeData ADT
 
-BASE_URL = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/"
-
-headers = {
-    'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-    'x-rapidapi-key': "..."
-    }
-
 
 def main():
+    """
+    Main function to show the work of RecipeSearch ADT and RecipeData ADT.
+    """
     try:
-        print("Hello it is an example module of using RecipeSearch ADT and RecipeData ADT")
+        print("It is an example module of using RecipeSearch ADT and RecipeData ADT")
         print("You can choose ComplexSearch or Meal generating")
         input_value = input("Input please which search you want to test(1, 2): ")
         if input_value == "1":
@@ -32,11 +25,14 @@ def main():
             include = "tomato"
             exclude = "olives"
             dish_type = "main course"
+            number_or_recipes = 5
             print(f"The search is carried out according to such parameters: the main ingredient is {option},"
                   f" cuisine is {cuisine}, diet is {diet}, ingredient to include is {include},"
-                  f"ingredient to exclude is {exclude} and the dish type is {dish_type}.")
-            more_options_complex = [option, cuisine, diet, include, exclude, dish_type, "50"]
-            data = get_data_from_url(BASE_URL, add_url, option, more_options=more_options_complex)
+                  f"ingredient to exclude is {exclude} and the dish type is {dish_type} and"
+                  f" the number of recipes is {number_or_recipes}.")
+            more_options_complex = [option, cuisine, diet, include, exclude, dish_type, "100"]
+            data = get_data_from_url(modules.main_functions.BASE_URL, add_url, option,
+                                     more_options=more_options_complex)
         elif input_value == "2":
             add_url = "mealplans/generate"
             option = None
@@ -47,18 +43,22 @@ def main():
                   f"the amount of calories is {calories},"
                   f" diet is {diet}, ingredient to exclude is {exclude}.")
             more_options_meal_gen = ["day", calories, diet, exclude]
-            data = get_data_from_url(BASE_URL, add_url, option, more_options=more_options_meal_gen)
+            data = get_data_from_url(modules.main_functions.BASE_URL, add_url, option,
+                                     more_options=more_options_meal_gen)
         else:
             raise ValueError
 
         if check_data(data) != "information OK":
             return "Such request is incorrect or there are not such recipes"
         else:
-            recipes = show_recipe_adt_work(add_url, data)
+            if add_url == "complexSearch":
+                recipes = show_recipe_adt_work(add_url, data, number_of_recipes=number_or_recipes)
+            else:
+                recipes = show_recipe_adt_work(add_url, data)
             res_ids = recipes.recipe_ids()
             res_names = recipes.recipe_names()
             res_amount = recipes.amount_of_recipes()
-            new_data = data_recipe(res_ids)
+            new_data = data_from_recipe_ids(res_ids)
             show_recipe_advanced_adt_work(new_data, res_names, res_amount)
             return "Everything went well"
 
@@ -68,19 +68,23 @@ def main():
         print("There is no data for such search, try again!")
 
 
-def show_recipe_adt_work(add_url, data):
+def show_recipe_adt_work(add_url, data, number_of_recipes=None):
     """
-    Shows the example of work of RecipeADT.
+    (str, dict, int) -> RecipeSearch
+
+    Shows the example of work of RecipeSearch ADT.
     """
     recipes = RecipeSearch(add_url, data)
     recipes.get_recipes()
-    recipes.delete_recipes()
-    print("There are such recipes for your query: ")
+    if add_url == "complexSearch":
+        recipes.recipe_selector(number_of_recipes)
+    print("There are such recipes for the query: ")
     recipes_names = recipes.recipe_names()
     for res in recipes_names:
         print(res)
+    print()
     if add_url == "mealplans/generate":
-        print("Information about the whole nutrition per day using mel generator")
+        print("Information about the whole nutrition per day using meal generator")
         nutrients = recipes.nutrients_per_day()
         str_plan = ""
         for item in nutrients:
@@ -97,6 +101,8 @@ def show_recipe_adt_work(add_url, data):
 
 def show_recipe_advanced_adt_work(data, recipes, amount_of_recipes):
     """
+    (dict, DynamicArray, int) -> None
+
     Function to show the work of RecipeAdvanced ADT.
     """
     recipes = RecipeData(data, recipes, amount_of_recipes)
@@ -113,114 +119,38 @@ def show_recipe_advanced_adt_work(data, recipes, amount_of_recipes):
     print("Here is main information about recipes: ")
     for recipe in all_recipes:
         cuisines = recipes.search_cuisines()[k]
-        if cuisines == []:
+        if cuisines == "no information":
             cuisines = "no one"
-        else:
-            cuisines = ", ".join(cuisines)
         diets = recipes.search_diets()[k]
-        if diets == []:
+        if diets == "no information":
             diets = "no one"
-        else:
-            diets = ", ".join(diets)
         types = recipes.search_types()[k]
-        types = ", ".join(types)
         new_str = f"{recipe} recipe is {res_cheap[k]}, {res_popularity[k]}, {res_health[k]}."
-        str_res = f"It contains of {res_calories[k]} calories, its health score is {res_health_score[k]}," \
-                  f" cooking time is {res_time_cook[k]} minutes."
+        str_res = f"It contains of {res_calories[k]}, its health score is {res_health_score[k]}," \
+                  f" cooking time is {res_time_cook[k]}."
         str_other_info = f"It belongs to such cuisines as {cuisines}, such diets as {diets} and" \
                          f" is sustainable for cooking as {types}."
         str_url = f"It's URL address is {res_urls[k]}"
         print(new_str)
         print(str_res)
         print(str_other_info)
-        recipe_ingredients = recipes.ingredients_data_per_recipe(k)
-        str_ingredients = "To cook this dish you need: " + ", ".join(recipe_ingredients) + '.'
+        recipe_ingredients = recipes.ingredients()
+        str_ingredients = "To cook this dish you need: " + ", ".join(recipe_ingredients[k]) + '.'
         print(str_ingredients)
         print(str_url)
         if k != len(all_recipes) - 1:
+            print()
             print("Next about another dish...")
             print()
         k += 1
 
     print("Next example of plotting nutrition of the first recipe:")
-    substances_res1 = recipes.main_substances_nutrition_percents\
-        (modules.default_information.MAIN_SUBSTANCES, 0)
-    plot_substances_info(substances_res1, "Main Substances").show()
-    other_substances_res1 = recipes.other_substances_nutrition_percents\
-        (modules.default_information.MAIN_SUBSTANCES, 0)
-    plot_substances_info(other_substances_res1, "Other Substances").show()
-
-
-def get_data_from_url(base_url, add_url, option, more_options=None):
-    """
-    Gets information using API.
-    """
-    if add_url == 'information':
-        full_url = base_url + str(option) + f"/information?includeNutrition=true"
-    elif add_url == 'complexSearch':
-        default_values = modules.default_information.DEFAULT_DICT_COMPLEX
-        querystring = {}
-        for i in range(len(more_options)):
-            key = default_values[i]
-            value = more_options[i]
-            if value != "None":
-                querystring[key] = value.lower()
-        full_url = base_url + "complexSearch"
-    elif add_url == 'mealplans/generate':
-        default_values = modules.default_information.DEFAULT_DICT_MEALS_VALUES
-        querystring = {}
-        for i in range(len(more_options)):
-            key = default_values[i]
-            value = more_options[i]
-            if value != "None":
-                querystring[key] = value
-        full_url = base_url + add_url
-
-    if add_url == "information":
-        response = requests.request("GET", full_url, headers=headers)
-    else:
-        response = requests.request("GET", full_url, headers=headers,
-                                    params=querystring)
-
-    data = json.loads(response.text)
-    return data
-
-
-def check_data(data):
-    """
-    Checks if the data is valid.
-    """
-    if data == []:
-        return "no data"
-    elif data == modules.errors.NO_RESULTS_COMPLEX or \
-            data == modules.errors.NO_RESULTS_MEALS:
-        return "no such results"
-    elif data == modules.errors.BAD_REQUEST:
-        return "bad request"
-    else:
-        return "information OK"
-
-
-def data_recipe(recipes_data):
-    """
-    Function that gets needed id numbers
-    for recipes data analysis.
-    """
-    recipes = []
-    for id in recipes_data:
-        data = get_data_from_url(BASE_URL, 'information', id)
-        recipes.append(data)
-    return recipes
-
-
-def plot_substances_info(substances, value):
-    """
-    Example of plotting information.
-    """
-    data_to_pandas = {value: substances[0], "Percents of daily needs": substances[1]}
-    data_to_plot = pd.DataFrame(data_to_pandas, columns=[value, "Percents of daily needs"])
-    data_plot = px.bar(data_to_plot, x=value, y="Percents of daily needs", color=value)
-    return data_plot
+    substances_res1 = recipes.main_nutrients_percents\
+        (modules.default_information.MAIN_NUTRIENTS, 0)
+    plot_nutrition(substances_res1, "Main nutrients").show()
+    other_substances_res1 = recipes.other_nutrients_percents\
+        (modules.default_information.MAIN_NUTRIENTS, 0)
+    plot_nutrition(other_substances_res1, "Vitamins, minerals, etc").show()
 
 
 print(main())
