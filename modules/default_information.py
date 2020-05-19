28a227addaf78aaf72505764bcda995d00105b57
Khristina Kokolus <60686300+khristinakokolus@users.noreply.github.com>
2020-05-19 18:57:53 +0300

    Update default_information.py

--- modules/default_information.py
+++ modules/default_information.py
@@ -1,20 +1,28 @@
 """Module that contains needed default variables"""
 
-DEFAULT_MAIN_INDICATORS = ["Calories", "TimeCooking", "healthyScore"]
+DEFAULT_INFO_URL = 'information'
 
-CUISINES = ["None", "African", "American", "British", "Cajun", "Carribean", "Chinese",
+DEFAULT_COMPLEX_URL = 'complexSearch'
+
+DEFAULT_MEAL_PLANNER_URL = 'mealplans/generate'
+
+DEFAULT_DICT_MEALS_VALUES = ["timeFrame", "targetCalories", "diet", "exclude"]
+
+DEFAULT_DICT_COMPLEX = ["query", "cuisine", "diet", "includeIngredients", "excludeIngredients", "type", "number"]
+
+CUISINES = ["African", "American", "British", "Cajun", "Carribean", "Chinese",
             "Eastern", "European", "French", "German", "Greek", "Indian", "Irish",
             "Italian", "Japanese", "Jewish", "Korean", "Latin American", "Mediterranean",
             "Mexican", "Middle Eastern", "Nordic", "Southern", "Spanish", "Thai", "Vietnamese"]
 
-DISH_TYPE = ["None", "main course", "side dish", "dessert", "appetizer", "salad", "bread",
+DISH_TYPE = ["main course", "side dish", "dessert", "appetizer", "salad", "bread",
              "breakfast", "soup", "beverage", "sauce", "marinade", "fingerfood", "snack", "drink"]
 
-DIETS = ["None", "gluten free", "ketogenic", "vegetarian", "lacto vegetarian", "ovo vegetarian",
+DIETS = ["gluten free", "ketogenic", "vegetarian", "lacto vegetarian", "ovo vegetarian",
          "vegan", "pescetarian", "paleo", "primal"]
 
 DEFAULT_MAIN_INFO = ["veryHealthy", "cheap", "veryPopular", "healthScore", "readyInMinutes", "sourceUrl",
                      "cuisines", "dishTypes", "diets"]
 
-MAIN_SUBSTANCES = ["Fat", "Saturated Fat", "Carbohydrates", "Net Carbohydrates",
+MAIN_NUTRIENTS = ["Fat", "Saturated Fat", "Carbohydrates", "Net Carbohydrates",
                    "Sugar", "Sodium", "Protein", "Fiber"]
